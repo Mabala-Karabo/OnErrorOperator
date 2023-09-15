597ea353c2b72441ccafd69be01060632834fa7b
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-15 15:40:03 +0200

    Add project files.

---
+++ OnErrorOperator/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        txtNum = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(611, 244)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(111, 72)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(133, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter a number"
+        ' 
+        ' txtNum
+        ' 
+        txtNum.Location = New Point(281, 69)
+        txtNum.Name = "txtNum"
+        txtNum.Size = New Size(150, 31)
+        txtNum.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtNum)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNum As TextBox
+End Class
---
+++ OnErrorOperator/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userInput As String
+        Dim numericValue As Double
+
+        ' Enable error handling
+        On Error GoTo ErrorHandler
+
+        ' Prompt the user for input
+        Console.Write("Enter a value: ")
+        userInput = txtNum.Text
+
+        ' Attempt to convert the user input to a numeric value
+        numericValue = Double.Parse(userInput)
+
+        ' If no error occurred, display the numeric value
+        MsgBox("The numeric value is: " & numericValue)
+
+ExitProgram:
+        ' Disable error handling
+        On Error Resume Next
+
+        ' Optional: Include cleanup or other code that runs after error handling
+
+        Console.ReadLine() ' Pause to see the result (optional)
+        Exit Sub
+
+ErrorHandler:
+        ' Handle the error when user input is not numeric
+        MsgBox("Error: Input is not numeric.")
+
+        ' Resume normal execution after handling the error
+        Resume ExitProgram
+    End Sub
+End Class
